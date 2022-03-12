7a064079fb47e5419544dec0c6c993edb1c91e1d
Mikki797 <40756204+Mikki797@users.noreply.github.com>
2022-03-12 10:48:35 +0300

    Add files via upload

---
+++ pages_spider.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class PagesSpider(scrapy.Spider):
+    name = "pages"
+
+    def start_requests(self):
+        urls = [
+            f'https://er.ru/party/program/?page={i}'
+            for i in range(2, 19)
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url[-2:]
+        if page[0] == '=':
+          page = page[1]
+
+        filename = f'page_{page}.txt'
+        with open(filename, 'w') as f:
+            f.write(response.xpath("//div[@class='download__title']/text()").get() + '\n')
+            
+            title = response.xpath("//p[@class='MsoNormal']//strong/span/text()").get()
+            if title is not None:
+              f.write(title + '\n')
+
+            for text in response.xpath("//p[@class='MsoNormal']/span/text()").getall():
+              f.write(text + '\n')
+
+        self.log(f'Saved file {filename}')
